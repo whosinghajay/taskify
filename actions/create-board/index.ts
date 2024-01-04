84b6097f0968bbdc45afaa6aff57f23ea41c3984
Ajay Singh <whosinghajay@gmail.com>
2024-01-04 09:07:56 +0530

    use action and form component created

---
+++ actions/create-board/index.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { CreateBoard } from "./schema";
+import { createSafeAction } from "@/lib/create-safe-action";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return {
+      errors: "Unauthorized",
+    };
+  }
+
+  const { title } = data;
+
+  let board;
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      errors: "Failed to create.",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+  return { data: board };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
