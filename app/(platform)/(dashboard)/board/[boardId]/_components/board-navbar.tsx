d456f2e7e9680a49eb21a4d8cbb620d83194ece0
Ajay Singh <whosinghajay@gmail.com>
2024-01-05 08:52:46 +0530

    Board Page created with all functionality

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -0,0 +1,20 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { Board } from "@prisma/client";
+import { BoardTitleForm } from "./board-title-form";
+import { BoardOptions } from "./board-options";
+
+interface BoardNavBarProps {
+  data: Board;
+}
+
+export const BoardNavBar = async ({ data }: BoardNavBarProps) => {
+  return (
+    <div className="w-full h-14 z-[40] bg-black/30 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id}/>
+      </div>
+    </div>
+  );
+};
