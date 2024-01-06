aba79476172e38378cfa05c70686fd01ef7c729d
Ajay Singh <whosinghajay@gmail.com>
2024-01-06 12:50:17 +0530

    card modal created successfully

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,6 +2,7 @@
 
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
+import { useCardModal } from "@/hooks/use-card-model";
 
 interface CardItemProps {
   index: number;
@@ -9,6 +10,8 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ index, data }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -17,6 +20,7 @@ export const CardItem = ({ index, data }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
